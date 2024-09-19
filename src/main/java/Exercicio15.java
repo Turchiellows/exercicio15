7f409276adcdc06176a15f20f225f6eb8d0e4ba8
root <root@DELL-7559>
2024-09-18 21:00:11 -0300

    Início do projeto

---
+++ src/main/java/Exercicio15.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class Exercicio15 {
+
+	public static void main(String[] args) {
+		// INSTANTIATE
+		Scanner sc = new Scanner(System.in);
+
+		// DECLARATE
+		String n;
+		int a;
+		int result;
+
+		// COLLECT DATA
+		System.out.print("Name : ");
+		n = sc.next();
+		System.out.print("Age : ");
+		a = sc.nextInt();
+
+		// OUTPUT
+		if (a >= 15 && a <= 25) {
+			System.out.println(n + " ACEITA");
+		} else {
+			System.out.println(n + " NÂO ACEITA");
+		}
+	}
+}
